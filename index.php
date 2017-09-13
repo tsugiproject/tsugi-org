6d8e6757462572a73f01b6d0b17a5523bc653ada
Charles Severance <csev@umich.edu>
2017-09-13 08:31:07 -0400

    Tweak the tm stuff

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ Knife (while supplies last).
 <p>
 Tsugi is a framework that handles much of the low-level detail of
 building multi-tenant tool that makes use of the 
-IMS Learning Tools Interoperability™ (LTI)™ and other learning
+IMS Learning Tools Interoperability® (LTI®) and other learning
 tool interoperability standards.
 <a href="http://www.tsugi.org" target="_blank">The Tsugi Framework</a> 
 provides library and database code to receive and model all
@@ -136,7 +136,7 @@ Tsugi is currently an
 <a href="https://www.apereo.org/incubation" target="_blank">incubation project</a> in the 
 <a href="http://www.apereo.org/" target="_new">Apereo Foundation</a>.
 <p>
-Learning Tools Interoperability™ (LTI™) is a
+Learning Tools Interoperability® (LTI®) is a
 trademark of <a href="http://www.imsglobal.org/" target="_blank">IMS Global Learning Consortium, Inc.</a>
 in the United States and/or other countries.
 </p>
