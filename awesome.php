2668f9c493df786ac0b13c9c0da6ea03d73ea888
Charles Severance <csev@umich.edu>
2019-07-12 13:29:02 -0700

    New file

---
+++ awesome.php
@@ -0,0 +1,31 @@
+<?php
+use \Tsugi\Core\LTIX;
+use \Tsugi\UI\Output;
+
+require_once "top.php";
+?>
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<?php
+require_once "nav.php";
+?>
+<div id="container">
+ <div id="page1" class="jumbotron">
+
+<p>
+This is a secret page that points to some pretty awesome things.
+</p>
+<ul>
+<li>
+<a href="https://www.tsugicloud.org/" target="_blank">Free Tsugi App Store</a>
+</li>
+<li>
+<a href="django.txt" target="_blank">Handout for the DJango Tsugi workshop</a>
+</li>
+<li>
+<a href="https://learningapps-dev.udayton.edu/tsugi/store/" target="_blank">
+https://learningapps-dev.udayton.edu/tsugi/store/
+</a>
+</li>
+</ul>
+</div>
+</div>
